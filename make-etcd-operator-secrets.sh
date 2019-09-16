92d8cf0dbbc550dae448b493c244164fb90eaba4
Cesar Wong <cewong@redhat.com>
2019-09-16 14:23:33 -0400

    fix templates dir

--- make-etcd-operator-secrets.sh
+++ make-etcd-operator-secrets.sh
@@ -17,5 +17,5 @@ for secret in etcd-client server peer; do
     
     export CRT=$(encode pki/${FILE}.pem)
     export KEY=$(encode pki/${FILE}-key.pem)
-    envsubst < templates/etcd-secret-template.yaml > etcd/${FILE}-tls-secret.yaml
+    envsubst < template/etcd-secret-template.yaml > etcd/${FILE}-tls-secret.yaml
 done
