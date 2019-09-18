eee462ce81bd11857aa72a3155c413187de9c9e5
Seth Jennings <sjenning@redhat.com>
2019-09-18 11:24:45 -0500

    enable CVO in render manifests and render to managed cluster

--- cluster-version-operator/render.sh
+++ cluster-version-operator/render.sh
@@ -4,7 +4,7 @@ set -eux
 
 source ../lib/common.sh
 
-cat > ../manifests/user/cluster-version-operator-secret.yaml <<EOF 
+cat > ../manifests/managed/cluster-version-operator-secret.yaml <<EOF 
 apiVersion: v1
 kind: Secret
 metadata:
@@ -13,4 +13,4 @@ data:
   kubeconfig: $(encode ../pki/admin.kubeconfig)
 EOF
 
-envsubst < cluster-version-operator-deployment.yaml > ../manifests/user/cluster-version-operator-deployment.yaml
+envsubst < cluster-version-operator-deployment.yaml > ../manifests/managed/cluster-version-operator-deployment.yaml
