e436ee6cdd9981f27a2907364a6a2ff72986d962
Seth Jennings <sjenning@redhat.com>
2019-09-25 10:49:27 -0500

    wire in oauthMetadata

--- kube-apiserver/render.sh
+++ kube-apiserver/render.sh
@@ -4,6 +4,33 @@ set -eux
 
 source ../lib/common.sh
 
+cat > oauthMetadata.json <<EOF
+{
+  "issuer": "https://${OAUTH_ROUTE}",
+  "authorization_endpoint": "https://${OAUTH_ROUTE}/oauth/authorize",
+  "token_endpoint": "https://${OAUTH_ROUTE}/oauth/token",
+  "scopes_supported": [
+    "user:check-access",
+    "user:full",
+    "user:info",
+    "user:list-projects",
+    "user:list-scoped-projects"
+  ],
+  "response_types_supported": [
+    "code",
+    "token"
+  ],
+  "grant_types_supported": [
+    "authorization_code",
+    "implicit"
+  ],
+  "code_challenge_methods_supported": [
+    "plain",
+    "S256"
+  ]
+}
+EOF
+
 cat > ../manifests/managed/kube-apiserver-secret.yaml <<EOF 
 apiVersion: v1
 kind: Secret
@@ -21,7 +48,10 @@ data:
   ca.crt: $(encode ../pki/combined-ca.pem)
   service-account.pub: $(encode ../pki/service-account.pem)
   config.yaml: $(encode config.yaml)
+  oauthMetadata: $(encode oauthMetadata.json)
 EOF
+
+rm -f oauthMetadata.json
 
 export HYPERKUBE_IMAGE=$(${CONTAINER_CLI} run -ti --rm ${RELEASE_IMAGE} image hyperkube)
 envsubst < kube-apiserver-deployment.yaml > ../manifests/managed/kube-apiserver-deployment.yaml
