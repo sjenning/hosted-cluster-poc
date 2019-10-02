5cf32fc09d787b9a54bc54baebe94248163c73da
markfranceschini <mark.franceschini1@ibm.com>
2019-10-02 15:50:17 -0500

    more testing

--- oauth-openshift-server/render.sh
+++ oauth-openshift-server/render.sh
@@ -8,27 +8,27 @@ CABUNDLE="$(encode ../pki/root-ca.pem)"
 
 # managed
 
-cat > ../manifests/managed/openshift-apiserver-secret.yaml <<EOF
+cat > ../manifests/managed/openshift-oauthserver-secret.yaml <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: openshift-apiserver
+  name: openshift-oauthserver
 data:
   kubeconfig: $(encode ../pki/service-admin.kubeconfig)
-  server.crt: $(encode ../pki/openshift-oauthserver-server.pem)
-  server.key: $(encode ../pki/openshift-oauthserver-server-key.pem)
+  server.crt: $(encode ../pki/oauth-openshift.pem)
+  server.key: $(encode ../pki/oauth-openshift-key.pem)
   etcd-client.crt: $(encode ../pki/etcd-client.pem)
   etcd-client.key: $(encode ../pki/etcd-client-key.pem)
   config.yaml: $(encode config.yaml)
   ca.crt: ${CABUNDLE}
 EOF
 
-export OPENSHIFT_APISERVER_IMAGE=$(${CONTAINER_CLI} run -ti --rm ${RELEASE_IMAGE} image oauth-server)
-envsubst < openshift-apiserver-deployment.yaml > ../manifests/managed/openshift-apiserver-deployment.yaml
+export OPENSHIFT_OAUTHSERVER_IMAGE=$(${CONTAINER_CLI} run -ti --rm ${RELEASE_IMAGE} image oauth-server)
+envsubst < openshift-oauthserver-deployment.yaml > ../manifests/managed/openshift-oauthserver-deployment.yaml
 cp openshift-oauthserver-service.yaml ../manifests/managed
 
 # user
 
-rm -f ../manifests/user/openshift-apiserver-apiservices.yaml
 
-cp openshift-apiserver-user-*.yaml ../manifests/user
+cp openshift-oauthserver-user-*.yaml ../manifests/user
+cp v4-*.yaml ../manifests/managed
