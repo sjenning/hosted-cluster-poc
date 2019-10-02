48ea45d288812477d84ba3c27969c7aa8031240b
Seth Jennings <sjenning@redhat.com>
2019-10-02 16:30:18 -0500

    add pull secret suppport allowing OCP content

--- ignition-generator/make-ignition.sh
+++ ignition-generator/make-ignition.sh
@@ -7,6 +7,9 @@ cp ../pki/kubelet-bootstrap.kubeconfig fake-root/etc/kubernetes/kubeconfig
 # kubeconfig needs to be world readable because network-operator reads it for server URL
 chmod +r fake-root/etc/kubernetes/kubeconfig
 cp ../pki/root-ca.pem fake-root/etc/kubernetes/ca.crt
+echo "copy pull secret"
+mkdir -p fake-root/var/lib/kubelet
+cp ../pull-secret fake-root/var/lib/kubelet/config.json
 echo "transpiling files"
 ./filetranspile -i base.ign -f fake-root -o tmp.ign
 echo "transpiling units"
--- render-manifests.sh
+++ render-manifests.sh
@@ -11,8 +11,15 @@ source config.sh
 rm -rf manifests
 mkdir -p manifests/managed manifests/user
 
+if [ ! -e pull-secret ]; then
+  echo "please provide a pull-secret file"
+  exit 1
+fi
+oc create secret generic pull-secret --from-file=.dockerconfigjson=pull-secret --type=kubernetes.io/dockerconfigjson -oyaml --dry-run > manifests/managed/00-pull-secret.yaml
+oc create secret generic pull-secret -n openshift-config --from-file=.dockerconfigjson=pull-secret --type=kubernetes.io/dockerconfigjson -oyaml --dry-run > manifests/user/00-pull-secret.yaml
+
 for component in etcd kube-apiserver kube-controller-manager kube-scheduler cluster-bootstrap openshift-apiserver openshift-controller-manager cluster-version-operator auto-approver user-manifests-bootstrapper; do
-    pushd ${component}
-    ./render.sh
-    popd
+  pushd ${component}
+  ./render.sh
+  popd
 done
