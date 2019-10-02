3f4e58ab93f5018b5e422d56a4a9b3f5c037f1a5
Seth Jennings <sjenning@redhat.com>
2019-10-02 12:26:56 -0500

    create admin demo user in patch-cluster

--- patch-cluster.sh
+++ patch-cluster.sh
@@ -10,3 +10,21 @@ oc patch ingresscontrollers default --type=merge -n openshift-ingress-operator -
 
 # image registry
 oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"storage":{"emptyDir":{}}}}'
+
+# create admin demo user with password "demo"
+oc create secret generic htpass-secret --from-literal=htpasswd=$(htpasswd -bnBC 10 demo demo | tr -d '\n') -n openshift-config
+cat << EOF | oc apply -f -
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: demo_htpasswd_provider
+    mappingMethod: claim
+    type: HTPasswd
+    htpasswd:
+      fileData:
+        name: htpass-secret
+EOF
+oc adm policy add-cluster-role-to-user cluster-admin demo
