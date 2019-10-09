7d812671de736f9c3cdb327f470d22598ffb7565
Seth Jennings <sjenning@redhat.com>
2019-10-09 10:32:00 -0500

    create kubeadmin account an improve installation msgs

--- install-openshift.sh
+++ install-openshift.sh
@@ -18,8 +18,8 @@ set -eu
 
 # make-pki.sh does not remove the /pki directory and does not regenerate certs that already exist.
 # If you wish to regenerate the PKI, remove the /pki directory.
-echo "Rendering PKI"
-./make-pki.sh >/dev/null
+echo "Creating PKI assets"
+./make-pki.sh &>/dev/null
 
 echo "Pulling release image"
 touch pull-secret
@@ -28,6 +28,9 @@ REGISTRY_AUTH_FILE=$(pwd)/pull-secret ${CONTAINER_CLI} pull ${RELEASE_IMAGE} >/d
 echo "Rendering manifests"
 rm -rf manifests
 mkdir -p manifests/managed manifests/user
+KUBEADMIN_PASSWORD=$(openssl rand -hex 24 | tr -d '\n')
+echo $KUBEADMIN_PASSWORD > kubeadmin-password
+oc create secret generic kubeadmin -n kube-system --from-literal=kubeadmin="$(htpasswd -bnBC 10 "" "${KUBEADMIN_PASSWORD}" | tr -d ':\n')" -oyaml --dry-run > manifests/user/kubeadmin-secret.yaml
 oc create secret generic pull-secret --from-file=.dockerconfigjson=pull-secret --type=kubernetes.io/dockerconfigjson -oyaml --dry-run > manifests/managed/pull-secret.yaml
 oc create secret generic pull-secret -n openshift-config --from-file=.dockerconfigjson=pull-secret --type=kubernetes.io/dockerconfigjson -oyaml --dry-run > manifests/user/00-pull-secret.yaml
 for component in etcd kube-apiserver kube-controller-manager kube-scheduler cluster-bootstrap openshift-apiserver openshift-controller-manager cluster-version-operator auto-approver ca-operator route-setter user-manifests-bootstrapper; do
@@ -37,24 +40,32 @@ for component in etcd kube-apiserver kube-controller-manager kube-scheduler clus
 done
 
 if [ "${PLATFORM}" != "none" ]; then
-  echo "Creating platform resources"
+  echo "Setting up platform resources"
   ./contrib/${PLATFORM}/setup.sh >/dev/null
 fi
 
-echo "Creating cluster"
+echo "Applying management cluster resources"
 # use `create ns` instead of `new-project` in case management cluster in not OCP
 oc create ns ${NAMESPACE} >/dev/null
 oc project ${NAMESPACE} >/dev/null
-cd manifests/managed
+pushd manifests/managed >/dev/null
 oc apply -f pull-secret.yaml >/dev/null
 oc secrets link default pull-secret --for=pull >/dev/null
 rm -f pull-secret.yaml
 oc apply -f . >/dev/null
+popd >/dev/null
 
-echo "Waiting for API to be healthy..."
+echo "Waiting up to 5m for the Kubernetes API at https://${EXTERNAL_API_DNS_NAME}:${EXTERNAL_API_PORT}"
 oc wait --for=condition=Available deployment/kube-apiserver --timeout=5m >/dev/null
 
-echo "Installation complete! The cluster is now ready for nodes to be added."
-echo "cluster-admin kubeconfig for cluster administration is at pki/admin.kubeconfig"
-echo "node-bootstrapper kubeconfig for joining nodes to the cluster is at pki/kubelet-bootstrap.kubeconfig "
-echo "Once nodes are added to the cluster, the remaining OpenShift components will deploy."
+echo "Waiting up to 15m for the cluster at https://${EXTERNAL_API_DNS_NAME}:${EXTERNAL_API_PORT} to initialize"
+export KUBECONFIG=$(pwd)/pki/admin.kubeconfig
+while ! oc wait --for=condition=Available clusterversion/version --timeout=15m &>/dev/null; do
+  sleep 10
+done
+
+echo "Install complete!"
+echo "To access the cluster as the system:admin user when using 'oc', run 'export $(pwd)/pki/admin.kubeconfig"
+echo "To join additional nodes to the cluster, use the node-bootstrapper kubeconfig at $(pwd)/pki/kubelet-bootstrap.kubeconfig"
+echo "Access the OpenShift web-console here: https://console-openshift-console.${INGRESS_SUBDOMAIN}"
+echo "Login into the console with user: kubeadmin, password ${KUBEADMIN_PASSWORD}"
