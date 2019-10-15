73b8b032540236b6ef5d95e8d104b96e43cae112
Seth Jennings <sjenning@redhat.com>
2019-10-15 16:05:24 -0500

    finish openvpn stuff, working now

--- openvpn/render.sh
+++ openvpn/render.sh
@@ -18,6 +18,14 @@ data:
   dh.pem: $(encode ../pki/openvpn-dh.pem)
   server.conf: $(encode server.conf)
 EOF
+cat > ../manifests/managed/openvpn-ccd-secret.yaml <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: openvpn-ccd
+data:
+  worker: $(encode worker)
+EOF
 cp openvpn-server-deployment.yaml ../manifests/managed/openvpn-server-deployment.yaml
 envsubst < openvpn-server-service.yaml > ../manifests/managed/openvpn-server-service.yaml
 
@@ -30,8 +38,8 @@ metadata:
   name: openvpn-client
   namespace: kube-system
 data:
-  tls.crt: $(encode ../pki/openvpn-client.pem)
-  tls.key: $(encode ../pki/openvpn-client-key.pem)
+  tls.crt: $(encode ../pki/openvpn-worker-client.pem)
+  tls.key: $(encode ../pki/openvpn-worker-client-key.pem)
   ca.crt: $(encode ../pki/openvpn-ca.pem)
   client.conf: $(encode client.conf.rendered)
 EOF
