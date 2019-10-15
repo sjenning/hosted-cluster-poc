86d01028ee6f35bd5d2ddb6f7280ea0d73fe72a5
ianjury <ianjury@ibm.com>
2019-10-15 15:08:11 -0500

    Adding cos cred

--- etcd/render.sh
+++ etcd/render.sh
@@ -24,5 +24,19 @@ data:
 EOF
 done
 
+envsubst < config_temp.txt > config.txt
+envsubst < credentials_temp.txt > credentials.txt
+
+cat > ../manifests/managed/cos-credentials.yaml <<EOF
+apiVersion: v1
+    kind: Secret
+    metadata:
+      name: cos-credentials
+    type: Opaque
+    data:
+      config: $(encode config.txt)
+      credentials: $(encode credentials.txt)
+EOF
+
 cp *.yaml ../manifests/managed
 envsubst < etcd-operator-cluster-role-binding.yaml > ../manifests/managed/etcd-operator-cluster-role-binding.yaml
