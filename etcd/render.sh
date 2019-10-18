0a08d052f5cd514880012da96588575a89ed2367
Kodie Glosser <krglosse@us.ibm.com>
2019-10-18 16:18:34 -0500

    role and bindings

--- etcd/render.sh
+++ etcd/render.sh
@@ -42,7 +42,6 @@ data:
 EOF
 
 cp *.yaml ../manifests/managed
-envsubst < etcd-operator-cluster-role-binding.yaml > ../manifests/managed/etcd-operator-cluster-role-binding.yaml
 envsubst < etcd-cluster.yaml > ../manifests/managed/etcd-cluster.yaml
 envsubst < etcd-cronjob.yaml > ../manifests/managed/etcd-cronjob.yaml
 envsubst < etcd-nodeport-service.yaml > ../manifests/managed/etcd-nodeport-service.yaml
