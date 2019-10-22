30183c04e25cbf8f22d76bcb8682049dbb46c963
Kodie Glosser <krglosse@us.ibm.com>
2019-10-22 17:07:41 -0500

    refactor cert signing

--- make-pki.sh
+++ make-pki.sh
@@ -12,34 +12,19 @@ function generate_client_key_cert() {
   file=$2
   user=$3
   org=$4
-  hostname="$5"
 
   if [ -f "${file}.pem" ]; then return 0; fi
 
-  cat > ${file}-csr.json <<EOF
-  {
-    "CN": "${user}",
-    "key": {
-      "algo": "rsa",
-      "size": 2048
-    },
-    "names": [
-      {
-        "O": "${org}"
-      }
-    ]
-  }
-EOF
+  envsubst < pki-cert-templates/${file}-csr.json > ${file}-csr.json
 
   cfssl gencert \
     -ca=${ca}.pem \
     -ca-key=${ca}-key.pem \
     -config=ca-config.json \
     -profile=kubernetes \
-    -hostname="${hostname}" \
     ${file}-csr.json | cfssljson -bare ${file}
 
-    rm ${file}-csr.json ${file}.csr
+    rm ${file}.csr
 }
 
 function generate_client_kubeconfig() {
@@ -108,23 +93,6 @@ function generate_secret() {
   envsubst < ../templates/secret-tls-template.yaml > ${file}-tls.yaml
 }
 
-# generate ca-config
-cat > ca-config.json <<EOF
-{
-  "signing": {
-    "default": {
-      "expiry": "8760h"
-    },
-    "profiles": {
-      "kubernetes": {
-        "usages": ["signing", "key encipherment", "server auth", "client auth"],
-        "expiry": "8760h"
-      }
-    }
-  }
-}
-EOF
-
 # generate CAs
 generate_ca "root-ca"
 generate_ca "cluster-signer"
@@ -149,20 +117,20 @@ fi
 generate_client_kubeconfig "root-ca" "kube-scheduler" "system:admin" "system:masters"
 
 # kube-apiserver
-generate_client_key_cert "root-ca" "kube-apiserver-server" "kubernetes" "kubernetes" "${EXTERNAL_API_DNS_NAME},${SERVICE_NETWORK_PREFIX}.0.1,${EXTERNAL_API_IP_ADDRESS},kubernetes,kubernetes.default.svc,kubernetes.default.svc.cluster.local,kube-apiserver,kube-apiserver.${NAMESPACE}.svc,kube-apiserver.${NAMESPACE}.svc.cluster.local"
+generate_client_key_cert "root-ca" "kube-apiserver-server" "kubernetes" "kubernetes"
 generate_client_key_cert "root-ca" "kube-apiserver-kubelet" "system:kube-apiserver" "kubernetes"
 generate_client_key_cert "root-ca" "kube-apiserver-aggregator-proxy-client" "system:openshift-aggregator" "kubernetes"
 
 # etcd
 generate_client_key_cert "root-ca" "etcd-client" "etcd-client" "kubernetes"
-generate_client_key_cert "root-ca" "etcd-server" "etcd-server" "kubernetes" "*.etcd.${NAMESPACE}.svc,etcd-client.${NAMESPACE}.svc,etcd,etcd-client,localhost"
-generate_client_key_cert "root-ca" "etcd-peer" "etcd-peer" "kubernetes" "*.etcd.${NAMESPACE}.svc,*.etcd.${NAMESPACE}.svc.cluster.local"
+generate_client_key_cert "root-ca" "etcd-server" "etcd-server" "kubernetes"
+generate_client_key_cert "root-ca" "etcd-peer" "etcd-peer" "kubernetes"
 
 # openshift-apiserver
-generate_client_key_cert "root-ca" "openshift-apiserver-server" "openshift-apiserver" "openshift" "openshift-apiserver,openshift-apiserver.${NAMESPACE}.svc,openshift-controller-manager.${NAMESPACE}.svc.cluster.local,openshift-apiserver.default.svc,openshift-apiserver.default.svc.cluster.local"
+generate_client_key_cert "root-ca" "openshift-apiserver-server" "openshift-apiserver" "openshift"
 
 # openshift-controller-manager
-generate_client_key_cert "root-ca" "openshift-controller-manager-server" "openshift-controller-manager" "openshift" "openshift-controller-manager,openshift-controller-manager.${NAMESPACE}.svc,openshift-controller-manager.${NAMESPACE}.svc.cluster.local"
+generate_client_key_cert "root-ca" "openshift-controller-manager-server" "openshift-controller-manager" "openshift"
 
 cat root-ca.pem cluster-signer.pem > combined-ca.pem
 
@@ -170,7 +138,7 @@ rm -f *.csr
 
 # openvpn assets
 generate_ca "openvpn-ca"
-generate_client_key_cert "openvpn-ca" "openvpn-server" "server" "kubernetes" "openvpn-server,openvpn-server.${NAMESPACE}.svc,${EXTERNAL_API_DNS_NAME}:${OPENVPN_NODEPORT}"
+generate_client_key_cert "openvpn-ca" "openvpn-server" "server" "kubernetes"
 generate_client_key_cert "openvpn-ca" "openvpn-kube-apiserver-client" "kube-apiserver" "kubernetes"
 generate_client_key_cert "openvpn-ca" "openvpn-worker-client" "worker" "kubernetes"
 if [ ! -e "openvpn-dh.pem" ]; then
