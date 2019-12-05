8717623747aff3aa9a49f6f1cd98be672c505c20
Akeem <alawal@us.ibm.com>
2019-12-04 23:35:15 -0600

    test repo

--- cluster-version-operator/render.sh
+++ cluster-version-operator/render.sh
@@ -4,7 +4,7 @@ set -eu
 
 source ../lib/common.sh
 
-cat > ../manifests/managed/cluster-version-operator-secret.yaml <<EOF 
+cat > ../manifests/managed/cluster-version-operator-secret.yaml <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
@@ -15,3 +15,4 @@ EOF
 
 envsubst < cluster-version-operator-deployment.yaml > ../manifests/managed/cluster-version-operator-deployment.yaml
 cp cluster-version-namespace.yaml ../manifests/user
+#test repo 
